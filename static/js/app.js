ef9f0f88579f1c51cf59b96b17b7fdffe66fa774
Alex <57154874+awbrown404@users.noreply.github.com>
2020-03-07 17:38:10 -0500

    part 1

---
+++ static/js/app.js
@@ -0,0 +1,39 @@
+// from data.js
+var tableData = data;
+
+var tbody = d3.select("tbody");
+
+data.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function () {
+    d3.event.preventDefault();
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select(".form-control");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+    console.log(filteredData);
+
+    var tbody = d3.select("tbody");
+    data.forEach((ufoReport) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
