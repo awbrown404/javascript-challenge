b039e8d84e223332cc5ce592a40e73a23d9829c7
Alex <57154874+awbrown404@users.noreply.github.com>
2020-03-07 20:06:40 -0500

    level 1 corrected/complete

--- Level 1/static/js/app.js
+++ Level 1/static/js/app.js
@@ -14,24 +14,25 @@ data.forEach((ufoReport) => {
 var button = d3.select("#filter-btn");
 
 button.on("click", function () {
+    // Remove exisiting table
+    d3.select("tbody").html("");
+    // Prevent default line
     d3.event.preventDefault();
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select(".form-control");
-
     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+    var inputValue = d3.select("#datetime").property("value");
 
     console.log(inputValue);
-    console.log(tableData);
 
     var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
 
     console.log(filteredData);
 
-    var tbody = d3.select("tbody");
-    data.forEach((ufoReport) => {
+    filteredData.forEach((ufoReport) => {
+
         var row = tbody.append("tr");
+
         Object.entries(ufoReport).forEach(([key, value]) => {
+            console.log(key, value)
             var cell = row.append("td");
             cell.text(value);
         });
